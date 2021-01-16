d32b3ebece95c32709b96390dde77551caee505e
jose david lombana <jose4125@gmail.com>
2021-01-15 21:40:44 -0500

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -9,7 +9,7 @@ which docker || sudo apt-get install -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" \
-   && sudo apt-get install docker-ce docker-ce-cli containerd.io && sudo usermod -aG docker jlombana
+   && sudo apt-get install docker-ce docker-ce-cli containerd.io && sudo usermod -aG docker ${USER} && newgrp docker
 
 which docker-compose || sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
 echo "zsh installation"
